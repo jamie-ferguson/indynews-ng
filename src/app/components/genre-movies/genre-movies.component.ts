10aa575748b18fddc251a49406353aee6b326f0e
Jamie Ferguson <jamiekferguson@gmail.com>
2019-03-19 00:03:28 +0000

    style improvements and removal of animation that meant app was breaking on Safari

--- src/app/components/genre-movies/genre-movies.component.ts
+++ src/app/components/genre-movies/genre-movies.component.ts
@@ -10,8 +10,7 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
   animations: [
     trigger('detailExpand', [
       state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      state('expanded', style({height: '*'}))
     ]),
   ],
 })
